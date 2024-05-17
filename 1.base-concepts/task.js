bb4b116009907eb403395e0ada8ca5c3715d0d39
xzotex <61540705+xzotex@users.noreply.github.com>
2024-05-17 19:48:30 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,25 +1,3 @@
-function solveEquation(a, b, c) {
-    'use strict'; // Активируем строгий режим
-
-    // Вычисляем дискриминант
-    const d = b * b - 4 * a * c;
-
-    if (d > 0) {
-        // Два различных корня
-        return [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
-    } else if (d == 0) {
-        // Один корень
-        return [-b / (2 * a)];
-    } else {
-        // Нет корней
-        return [];
-    }
-}
-
-
-
-
-
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
