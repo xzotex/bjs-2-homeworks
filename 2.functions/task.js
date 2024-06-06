421772b65d41e1c968f3f81537b04416798ab44e
xzotex <61540705+xzotex@users.noreply.github.com>
2024-06-06 17:23:37 +0300

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,18 @@
 function getArrayParams(...arr) {
-    let min = Infinity; 
-    let max = -Infinity; 
-    let sum = 0; 
-
-
-    arr.forEach(element => {
-        if (element > max) {
-            max = element; 
-        }
-        if (element < min) {
-            min = element;
-        }
-        sum += element;
-    });
-
-
-    const avg = sum / arr.length;
-
-
-    return { min: Math.round(min), max: Math.round(max), avg: parseFloat(avg.toFixed(2)) };
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    sum += arr[i];
+  }
+  let avg = Number((sum / arr.length).toFixed(2));
+  return { min: min, max: max, avg: avg };
 }
 
 
