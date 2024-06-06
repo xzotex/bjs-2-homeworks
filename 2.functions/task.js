c6ecdca97d77d3e69d11efb0e77e60fe42ea4b23
xzotex <61540705+xzotex@users.noreply.github.com>
2024-06-06 14:56:41 +0300

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -21,27 +21,42 @@ function getArrayParams(...arr) {
     return { min: Math.round(min), max: Math.round(max), avg: parseFloat(avg.toFixed(2)) };
 }
 
-function getArrayParams(...arr) {
-
-  return { min: min, max: max, avg: avg };
-}
 
 function summElementsWorker(...arr) {
-
+     if (!arr.length) return 0;
+  return arr.reduce((accumulator, currentValue) => accumulator + currentValue);
 }
 
 function differenceMaxMinWorker(...arr) {
-
+    if (!arr.length) return 0; // Проверка наличия элементов
+    const max = Math.max(...arr);
+    const min = Math.min(...arr);
+    return max - min;
 }
 
 function differenceEvenOddWorker(...arr) {
-
+     if (!arr.length) return 0; // Проверка наличия элементов
+  let sumEven = 0;
+  let sumOdd = 0;
+  arr.forEach(arr => {
+    if (element % 2 === 0) {
+      sumEven += element;
+    } else {
+      sumOdd += element;
+    }
+  });
+  return sumEven - sumOdd;
 }
 
 function averageEvenElementsWorker(...arr) {
-
-}
-
-function makeWork (arrOfArr, func) {
-
+    if (!arr.length) return 0; // Проверка наличия элементов
+  let sumEven = 0;
+  let countEven = 0;
+  arr.forEach(element => {
+    if (element % 2 === 0) {
+      sumEven += element;
+      countEven++;
+    }
+  });
+  return sumEven / countEven;
 }
